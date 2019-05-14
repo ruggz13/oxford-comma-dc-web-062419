14ff296bf06903e5d28a8e41f16c5a60acf8a8ec
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-14 21:39:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,8 @@
 def oxford_comma(array)
   if array.size == 1
     string = array.join
-    return string
   elsif array.size == 2
     string = array.join(" and ")
-    return string
   else
     string = array[0..-2].join(", ") + ", and " + array[-1]
   end
